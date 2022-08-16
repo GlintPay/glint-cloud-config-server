041a6183e819c9427946cc48be8b6aada7fafc1e
Andrew Regan <aregan@gmail.com>
2022-08-16 15:29:18 +0100

    Rename

--- api/precedence_sorting_test.go
+++ api/precedence_sorting_test.go
@@ -12,36 +12,36 @@ func TestBasic(t *testing.T) {
 	expected := []PropertySource{
 		{Name: "application.yml", Source: EmptySource},
 		{Name: "application-junk.yml", Source: EmptySource},
-		{Name: "application-staging.yml", Source: EmptySource},
+		{Name: "application-test.yml", Source: EmptySource},
 		{Name: "application-demo.yml", Source: EmptySource},
-		{Name: "application-staging-us.yml", Source: EmptySource},
+		{Name: "application-test-us.yml", Source: EmptySource},
 		{Name: "application-demo-us.yml", Source: EmptySource},
 
-		{Name: "adjust-events.yml", Source: EmptySource},
-		{Name: "adjust-events-junk.yml", Source: EmptySource},
-		{Name: "adjust-events-staging.yml", Source: EmptySource},
-		{Name: "adjust-events-demo.yml", Source: EmptySource},
-		{Name: "adjust-events-staging-us.yml", Source: EmptySource},
-		{Name: "adjust-events-demo-us.yml", Source: EmptySource},
+		{Name: "other-service.yml", Source: EmptySource},
+		{Name: "other-service-junk.yml", Source: EmptySource},
+		{Name: "other-service-test.yml", Source: EmptySource},
+		{Name: "other-service-demo.yml", Source: EmptySource},
+		{Name: "other-service-test-us.yml", Source: EmptySource},
+		{Name: "other-service-demo-us.yml", Source: EmptySource},
 	}
 
 	// Ordering of inputs is not random in reality; this is just to clarify the effect of the sorting algo
 	sources := []PropertySource{
-		{Name: "adjust-events-demo-us.yml", Source: EmptySource},
+		{Name: "other-service-demo-us.yml", Source: EmptySource},
 		{Name: "application-junk.yml", Source: EmptySource},
-		{Name: "adjust-events-staging-us.yml", Source: EmptySource},
+		{Name: "other-service-test-us.yml", Source: EmptySource},
 		{Name: "application-demo-us.yml", Source: EmptySource},
-		{Name: "adjust-events-staging.yml", Source: EmptySource},
+		{Name: "other-service-test.yml", Source: EmptySource},
 		{Name: "application-demo.yml", Source: EmptySource},
-		{Name: "adjust-events-demo.yml", Source: EmptySource},
-		{Name: "adjust-events.yml", Source: EmptySource},
-		{Name: "adjust-events-junk.yml", Source: EmptySource},
-		{Name: "application-staging-us.yml", Source: EmptySource},
-		{Name: "application-staging.yml", Source: EmptySource},
+		{Name: "other-service-demo.yml", Source: EmptySource},
+		{Name: "other-service.yml", Source: EmptySource},
+		{Name: "other-service-junk.yml", Source: EmptySource},
+		{Name: "application-test-us.yml", Source: EmptySource},
+		{Name: "application-test.yml", Source: EmptySource},
 		{Name: "application.yml", Source: EmptySource},
 	}
 
-	sorter := Sorter{AppNames: []string{"adjust-events"}, Profiles: []string{"demo-us", "staging-us", "demo", "staging"}, Sources: sources}
+	sorter := Sorter{AppNames: []string{"other-service"}, Profiles: []string{"demo-us", "test-us", "demo", "test"}, Sources: sources}
 	sort.SliceStable(sources, sorter.Sort())
 
 	assert.Equal(t, expected, sources)
